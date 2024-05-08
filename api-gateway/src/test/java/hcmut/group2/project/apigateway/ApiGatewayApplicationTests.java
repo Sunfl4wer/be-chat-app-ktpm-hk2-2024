222c42ec13f4677fa988ec8ed36da6d31e465f23
son.nguyen <sonnt13@vng.com.vn>
2024-05-08 14:37:56 +0700

    chore: add code

---
+++ api-gateway/src/test/java/hcmut/group2/project/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package hcmut.group2.project.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
